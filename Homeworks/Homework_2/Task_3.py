96af634ff62fb85ef52302d768cbc4d41a0655aa
Valery Chyrok <C:\Users\vakac\AppData\Roaming\The Bat!>
2023-01-21 16:58:47 +0300

    Added Homework_6: tasts've been revised provided list comprehension, map(), filter(), list(), sum()

--- Homeworks/Homework_2/Task_3.py
+++ Homeworks/Homework_2/Task_3.py
@@ -1,17 +1,32 @@
 
 # Задайте список из n чисел последовательности (1+1/n)**n и выведите на экран их сумму.
 
-l = list()
-sum = 0
+# ------------------------------------------------------ #
+# первый вариант решения
+
+
+# l = list()
+# sum = 0
+
+# n = int(input("Enter N: "))
+
+
+# for i in range(1, n+1):
+#     l.append(round((1+1/i)**(i), 3))
+
+# for i in l:
+#     sum += i
+
+# # print(l)
+# print(f"Sum of the first {n} items of the given series (1+1/n)**n: {sum}")
+
+# ------------------------------------------------------ #
+# второй вариант решения
+
 
 n = int(input("Enter N: "))
 
-
-for i in range(1, n+1):
-    l.append(round((1+1/i)**(i), 3))
-
-for i in l:
-    sum += i
-
-# print(l)
-print(f"Sum of the first {n} items of the given series (1+1/n)**n: {sum}")
+l = map(lambda i: (1+1/i)**i,range(1, n+1))
+add=sum(l)
+print(f"Sum of the first {n} items of the given series (1+1/n)**n: {add}")
+# ------------------------------------------------------ #
--- Homeworks/Homework_3/Task_1.py
+++ Homeworks/Homework_3/Task_1.py
@@ -9,8 +9,12 @@
 import random
 
 c = int(input("Enter a number of items in a list: "))
+# ------------------------------------------------------ #
+# первый вариант решения
+
+
 l = list()
-mult = 1
+add = 0
 
 for i in range(c):
     l.append(random.randint(0, 5*c))
@@ -18,7 +22,21 @@ for i in range(c):
 print(l)
 
 for i in range(len(l)):
-    if (i % 2 == 0):
-        mult *= l[i]
+    if (i % 2 == 0): # нечетная позиция, а индекс - четный таким образом!
+        add += l[i]
 
-print(f"Result of multiplication of odd items of the list {l} is: {mult}")
+print(add)
+# ------------------------------------------------------ #
+# второй вариант решения
+
+
+l=list(l[i] for i in range(len(l)) if i%2==0) # нечетная позиция, а индекс - четный таким образом!
+
+print(sum(l))
+# ------------------------------------------------------ #
+# можно срезом решить (из семинара)
+
+
+print(sum(l[0::2])) # нечетная позиция, а индекс - четный таким образом!
+
+
